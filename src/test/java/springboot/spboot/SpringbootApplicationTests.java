93f3615e6c7e65ebe144cf12fb22f0c5fc9461c1
prasanthghub <prasanthmeduri58@gmail.com>
2023-04-10 16:40:06 +0530

    adding code to git

---
+++ src/test/java/springboot/spboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
